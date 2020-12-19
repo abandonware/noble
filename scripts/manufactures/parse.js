813e69a6b64c900d86a930f45cd2fb699c4603a7
Tino Wehe <tino.wehe@aperto.de>
2020-12-19 17:13:17 +0100

    154 - Add manufacturer data as single PR/ commit

---
+++ scripts/manufactures/parse.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const parser = require('csv-parser');
+const fs = require('fs');
+
+const CSV_PATH = path.join(process.cwd(), 'scripts', 'manufactures', 'data.csv');
+const HEX_OUT_PATH = path.join(process.cwd(), 'lib', 'manufactures-hex.json');
+const DEC_OUT_PATH = path.join(process.cwd(), 'lib', 'manufactures-dec.json');
+
+const escape = str => {
+  return str
+    .replace(/[\\]/g, '\\\\')
+    .replace(/["]/g, '\\"')
+    .replace(/[/]/g, '\\/')
+    .replace(/[\b]/g, '\\b')
+    .replace(/[\f]/g, '\\f')
+    .replace(/[\n]/g, '\\n')
+    .replace(/[\r]/g, '\\r')
+    .replace(/[\t]/g, '\\t');
+};
+
+const hexResults = {};
+const decResults = {};
+
+fs.createReadStream(CSV_PATH)
+  .pipe(parser())
+  .on('data', data => {
+    hexResults[data.Hexadecimal] = escape(data.Company);
+    decResults[data.Decimal] = escape(data.Company);
+  })
+  .on('end', () => {
+    try {
+      fs.writeFileSync(HEX_OUT_PATH, JSON.stringify(hexResults, null, '\t'), { encoding: 'utf-8' });
+      fs.writeFileSync(DEC_OUT_PATH, JSON.stringify(decResults, null, '\t'), { encoding: 'utf-8' });
+    } catch (e) {
+      console.error(e);
+    }
+  });
