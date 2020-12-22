f4ef6db1d2e57041effdb44039f5592d539373a4
Tino Wehe <tino.wehe@aperto.de>
2020-12-22 14:59:35 +0100

    #154 - Remove "data.csv" due to possible licence issues

--- scripts/manufactures/README.md
+++ scripts/manufactures/README.md
@@ -1,5 +1,9 @@
 #Manufactures
 
-from: https://www.bluetooth.com/specifications/assigned-numbers/company-identifiers/
+## Steps
 
-last update: 2020-12-19T16:12:56.492Z
+1.) Downlaod a CSV list of all manufacurers from https://www.bluetooth.com/specifications/assigned-numbers/company-identifiers/
+2.) save the CSV data into ```scripts/manufactures/data.csv```
+3.) run ```yarn parse-manufactures```
+
+last update: 2020-12-19
--- scripts/manufactures/parse.js
+++ scripts/manufactures/parse.js
@@ -5,7 +5,6 @@ const fs = require('fs');
 const CSV_PATH = path.join(process.cwd(), 'scripts', 'manufactures', 'data.csv');
 const HEX_OUT_PATH = path.join(process.cwd(), 'lib', 'manufactures-hex.json');
 const DEC_OUT_PATH = path.join(process.cwd(), 'lib', 'manufactures-dec.json');
-const README_PATH = path.join(process.cwd(), 'scripts', 'manufactures', 'README.md');
 
 const escape = str => {
   return str
@@ -32,7 +31,6 @@ fs.createReadStream(CSV_PATH)
     try {
       fs.writeFileSync(HEX_OUT_PATH, JSON.stringify(hexResults, null, '\t'), { encoding: 'utf-8' });
       fs.writeFileSync(DEC_OUT_PATH, JSON.stringify(decResults, null, '\t'), { encoding: 'utf-8' });
-      fs.writeFileSync(README_PATH, ['#Manufactures', '', 'from: https://www.bluetooth.com/specifications/assigned-numbers/company-identifiers/', '', `last update: ${new Date().toISOString()}`].join('\n'));
     } catch (e) {
       console.error(e);
     }
