abandonware/noble

commit 2af44d809337e14bb527bca08e5d75ca829365b6

Add "manufacturer" prop to "Peripheral",
- add script to parse Manufacturer Data from https://www.bluetooth.com/specifications/assigned-numbers/company-identifiers/

lib/manufacture.js
@@ -1,0 +1,46 @@
+const tryRequire = modulePath => {
+  try {
+    return require(modulePath);
+  } catch (e) {
+    console.error(`ERROR:\tCould not load manufacturer list from "${modulePath}".`);
+    console.error(`\tPlease see "${__dirname}/../scripts/manufactures/README.md" for instructions.`);
+    return {};
+  }
+};
+
+const hexValues = tryRequire('./manufactures-hex.json');
+const decValues = tryRequire('./manufactures-dec.json');
+
+function Manufacture (noble, data) {
+  this._noble = noble;
+
+  this.name = null;
+  this.data = data;
+
+  try {
+    const tmp = JSON.parse(JSON.stringify(data)).data;
+
+    if (Array.isArray(tmp)) {
+      this.name = decValues[tmp[0]];
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+Manufacture.prototype.toString = function () {
+  return JSON.stringify({
+    name: this.name,
+    data: JSON.parse(JSON.stringify(this.data))
+  });
+};
+
+Manufacture.nameFromHex = function (hexValue) {
+  return hexValues[hexValue] || undefined;
+};
+
+Manufacture.nameFromDec = function (decValue) {
+  return decValues[decValue] || undefined;
+};
+
+module.exports = Manufacture;

test/test-manufacture.js
@@ -1,0 +1,67 @@
+require('should');
+const sinon = require('sinon');
+
+const tryRequire = modulePath => {
+  try {
+    return require(modulePath);
+  } catch (e) {
+    console.error(`ERROR:\tCould not load manufacturer list from "${modulePath}".`);
+    console.error(`\tPlease see "${__dirname}/../scripts/manufactures/README.md" for instructions.`);
+    return false;
+  }
+};
+
+const Manufacture = require('../lib/manufacture');
+
+const manufacturersCodesHex = tryRequire('../lib/manufactures-hex.json');
+const manufacturersCodesDec = tryRequire('../lib/manufactures-dec.json');
+
+const GARMIN_MAN_NAME = 'Garmin International, Inc.';
+const GARMIN_MAN_HEX = '87 00 0d 9c';
+const APPLE_MAN_NAME = 'Apple, Inc.';
+const APPLE_MAN_HEX = '4c 00 10 07 13 1f 79 82 1c 76 18';
+
+describe('Manufacture', function () {
+  const mockDataGarmin = Buffer.from(GARMIN_MAN_HEX.replace(/\s+/g, ''), 'hex');
+  const mockDataApple = Buffer.from(APPLE_MAN_HEX.replace(/\s+/g, ''), 'hex');
+  let mockNoble = null;
+
+  let manufactureGarmin = null;
+  let manufactureApple = null;
+
+  beforeEach(function () {
+    if (manufacturersCodesDec === false || manufacturersCodesHex === false) {
+      this.skip();
+    }
+
+    mockNoble = {
+      readValue: sinon.spy(),
+      writeValue: sinon.spy()
+    };
+
+    manufactureGarmin = new Manufacture(mockNoble, mockDataGarmin);
+    manufactureApple = new Manufacture(mockNoble, mockDataApple);
+  });
+
+  afterEach(function () {
+    manufactureGarmin = null;
+    manufactureApple = null;
+  });
+
+  it('should have data', function () {
+    manufactureGarmin.data.should.equal(mockDataGarmin);
+    manufactureApple.data.should.equal(mockDataApple);
+  });
+
+  it(`should have a name "${GARMIN_MAN_NAME}" from data`, function () {
+    manufactureGarmin = new Manufacture(mockNoble, mockDataGarmin);
+    manufactureGarmin.name.should.equal(GARMIN_MAN_NAME);
+    Manufacture.nameFromDec(mockDataGarmin[0]).should.equal(GARMIN_MAN_NAME);
+  });
+
+  it(`should have a name "${APPLE_MAN_NAME}" from data`, function () {
+    manufactureApple = new Manufacture(mockNoble, mockDataApple);
+    manufactureApple.name.should.equal(APPLE_MAN_NAME);
+    Manufacture.nameFromDec(mockDataApple[0]).should.equal(APPLE_MAN_NAME);
+  });
+});

lib/peripheral.js
@@ -1,6 +1,8 @@
 const events = require('events');
 const util = require('util');
 
+const Manufacturer = require('./manufacture');
+
 function Peripheral (noble, id, address, addressType, connectable, advertisement, rssi) {
   this._noble = noble;
 
@@ -14,6 +16,11 @@ function Peripheral (noble, id, address, addressType, connectable, advertisement
   this.services = null;
   this.mtu = null;
   this.state = 'disconnected';
+  this.manufacturer = null;
+
+  if (typeof this.advertisement !== 'undefined' && typeof this.advertisement.manufacturerData !== 'undefined') {
+    this.manufacturer = new Manufacturer(noble, this.advertisement.manufacturerData);
+  }
 }
 
 util.inherits(Peripheral, events.EventEmitter);

commit 54c4e80c176d98ca7ed68afa037aa75407ca3712

Fix lint errors
This patch fixed lint errors introduced by 2af44d809337.

Test: npm run lint

lib/manufacture.js
@@ -1,9 +1,11 @@
+const path = require('path');
+
 const tryRequire = modulePath => {
   try {
     return require(modulePath);
   } catch (e) {
     console.error(`ERROR:\tCould not load manufacturer list from "${modulePath}".`);
-    console.error(`\tPlease see "${__dirname}/../scripts/manufactures/README.md" for instructions.`);
+    console.error(`\tPlease see "${path.join(__dirname, '..', 'scripts', 'manufactures', 'README.md')}" for instructions.`);
     return {};
   }
 };

test/test-manufacture.js
@@ -1,3 +1,4 @@
+const path = require('path');
 require('should');
 const sinon = require('sinon');
 
@@ -6,7 +7,7 @@ const tryRequire = modulePath => {
     return require(modulePath);
   } catch (e) {
     console.error(`ERROR:\tCould not load manufacturer list from "${modulePath}".`);
-    console.error(`\tPlease see "${__dirname}/../scripts/manufactures/README.md" for instructions.`);
+    console.error(`\tPlease see "${path.join(__dirname, '..', 'scripts', 'manufactures', 'README.md')}" for instructions.`);
     return false;
   }
 };